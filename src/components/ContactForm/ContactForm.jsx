12f1c94a1f22fe0a5a9eac761cf5082181ca9b15
Olya Vorontsova <16olya1996@gmail.com>
2025-03-09 22:29:14 +0100

    work on form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,41 @@
-import { Formik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { nanoid } from "nanoid";
+
+import style from "./ContactForm.module.css";
 
 export default function ContactForm() {
+  const nameId = nanoid();
+  const numberId = nanoid();
+
   return (
     <Formik>
-      <p>Test</p>
+      <Form className={style.form}>
+        <div className={style.fieldWrapper}>
+          <label className={style.label} htmlFor="nameId">
+            Name
+          </label>
+          <Field
+            className={style.field}
+            type="text"
+            name="username"
+            id="nameId"
+          ></Field>
+        </div>
+        <div className={style.fieldWrapper}>
+          <label className={style.label} htmlFor="numberId">
+            Number
+          </label>
+          <Field
+            className={style.field}
+            type="tel"
+            name="number"
+            id="numberId"
+          ></Field>
+        </div>
+        <button className={style.button} type="submit">
+          Add contact
+        </button>
+      </Form>
     </Formik>
   );
 }
