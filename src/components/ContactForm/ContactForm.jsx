7d1e58e287af18977ce8dc059114d1fb0b736883
Olya Vorontsova <16olya1996@gmail.com>
2025-03-10 23:20:35 +0100

    add on validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,14 +1,28 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { nanoid } from "nanoid";
+import * as Yup from "yup";
 
 import style from "./ContactForm.module.css";
+
+const newUserSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Your name is too short")
+    .max(50, "Your name is to long")
+    .required("This field is required"),
+  number: Yup.string()
+    .min(3, "Your telephone number is too short")
+    .max(50, "Your telephone number is too long")
+    .required("This field is required"),
+});
+
+const initialNewUser = { name: "", number: "" };
 
 export default function ContactForm() {
   const nameId = nanoid();
   const numberId = nanoid();
 
   return (
-    <Formik>
+    <Formik initialValues={initialNewUser} validationSchema={newUserSchema}>
       <Form className={style.form}>
         <div className={style.fieldWrapper}>
           <label className={style.label} htmlFor="nameId">
@@ -17,9 +31,10 @@ export default function ContactForm() {
           <Field
             className={style.field}
             type="text"
-            name="username"
+            name="name"
             id="nameId"
           ></Field>
+          <ErrorMessage name="name" component="span" />
         </div>
         <div className={style.fieldWrapper}>
           <label className={style.label} htmlFor="numberId">
@@ -31,6 +46,7 @@ export default function ContactForm() {
             name="number"
             id="numberId"
           ></Field>
+          <ErrorMessage name="number" component="span" />
         </div>
         <button className={style.button} type="submit">
           Add contact
